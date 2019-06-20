8ad6a5a803af8cd5e7e06dc0b126ebb03f57ec77
Docent <Docent@PC06I41>
2019-06-20 12:19:30 +0200

    Copia de mig matí

--- PLA4 Eclipse/scriptsPLA4.js
+++ PLA4 Eclipse/scriptsPLA4.js
@@ -5,19 +5,19 @@
 function exercici1() {
 	var inputCadena = "ABACDAEFAGHAABAH";
 	var numeroCoincidencies = 0;
-	for (posicioLletra = 1; posicioLletra <= inputCadena.length; posicioLletra++) {
+	for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
 		if (inputCadena.charAt(posicioLletra) == "A") {
 			numeroCoincidencies++;
 		}
 	}
-	return numeroCoincidencies
+	return numeroCoincidencies;
 }
 
 function exercici2(inputUsuari){
 	var inputCadena = "ABACDAEFAGHAABAH"
 		var numeroCoincidencies = 0;
 		inputLletra = inputUsuari;
-		for (posicioLletra = 1; posicioLletra <= inputCadena.length; posicioLletra++) {
+		for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
 			if (inputCadena.charAt(posicioLletra) == inputLletra) {
 				numeroCoincidencies++
 			}
@@ -29,7 +29,7 @@ function exercici3(inputUsuari1, inputUsuari2){
 	var inputCadena = inputUsuari1
 		var numeroCoincidencies = 0;
 		inputLletra = inputUsuari2;
-		for (posicioLletra = 1; posicioLletra <= inputCadena.length; posicioLletra++) {
+		for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
 			if (inputCadena.charAt(posicioLletra) == inputLletra) {
 				numeroCoincidencies++
 			}
