97b4c080adb37f6685d830c889385df776ece13e
Docent <Docent@PC06I41>
2019-06-20 13:28:46 +0200

    Final de dia de 20-06

--- PLA4 Eclipse/scriptsPLA4.js
+++ PLA4 Eclipse/scriptsPLA4.js
@@ -13,26 +13,64 @@ function exercici1() {
 	return numeroCoincidencies;
 }
 
-function exercici2(inputUsuari){
+function exercici2(inputUsuari) {
 	var inputCadena = "ABACDAEFAGHAABAH"
-		var numeroCoincidencies = 0;
-		inputLletra = inputUsuari;
-		for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
-			if (inputCadena.charAt(posicioLletra) == inputLletra) {
-				numeroCoincidencies++
-			}
+	var inputLletra = inputUsuari;
+	var numeroCoincidencies = 0;
+	for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
+		if (inputCadena.charAt(posicioLletra) == inputLletra) {
+			numeroCoincidencies++;
 		}
-		return numeroCoincidencies
 	}
+	return numeroCoincidencies;
+}
 
-function exercici3(inputUsuari1, inputUsuari2){
+function exercici3(inputUsuari1, inputUsuari2) {
 	var inputCadena = inputUsuari1
-		var numeroCoincidencies = 0;
-		inputLletra = inputUsuari2;
-		for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
-			if (inputCadena.charAt(posicioLletra) == inputLletra) {
-				numeroCoincidencies++
-			}
+	var inputLletra = inputUsuari2;
+	var numeroCoincidencies = 0;
+	for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
+		if (inputCadena.charAt(posicioLletra) == inputLletra) {
+			numeroCoincidencies++;
 		}
-		return numeroCoincidencies
 	}
+	return numeroCoincidencies
+}
+
+function exercici4(inputUsuari1, inputUsuari2) {
+	var inputCadena = inputUsuari1;
+	var inputLletra = inputUsuari2;
+	var lletraLocalitzada = 0;
+	for (posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
+		if (inputCadena.charAt(posicioLletra) == inputLletra
+				&& lletraLocalitzada == 0) {
+			lletraLocalitzada = posicioLletra + 1;
+		}
+	}
+	return lletraLocalitzada;
+}
+
+function exercici6(inputUsuari1, inputUsuari2) {
+	var inputCadena = inputUsuari1
+	var inputLletra = inputUsuari2
+	var llistaPosicions = [ "primera", "segona", "tercera", "quarta",
+			"cinquena", "sisena", "setena", "vuitena", "novena", "desena",
+			"onzena", "dotzena", "tretzena", "catorzena", "quinzena",
+			"setzena", "dissetena", "divuitena", "dinovena", "vintena" ];
+	var llistaResposta = [];
+	for (var posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++) {
+		if (inputCadena.charAt(posicioLletra) == inputLletra) {
+			llistaResposta.push(llistaPosicions[posicioLletra]);
+		}
+	}
+	return llistaResposta.join();
+}
+
+function exercici7(input1, input2){
+	var cadena = input1
+	var numero = input1
+		for (var posicioLletra = 0; posicioLletra <= inputCadena.length; posicioLletra++){
+		
+	}
+return 	
+}
