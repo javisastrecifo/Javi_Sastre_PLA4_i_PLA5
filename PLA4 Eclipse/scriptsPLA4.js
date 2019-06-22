ba56aa4a3ac9a0c0131e6cab92b496f7994ef915
javi <javi@javi-Dell>
2019-06-22 23:34:38 +0200

    Dissabte  nit

--- PLA4 Eclipse/scriptsPLA4.js
+++ PLA4 Eclipse/scriptsPLA4.js
@@ -63,20 +63,26 @@ function exercici6(inputUsuari1, inputUsuari2) {
 		if (inputCadena.charAt(posicioLletra) == inputLletra) {
 			llistaResposta.push(llistaPosicions[posicioLletra]);
 		}
-		if (llistaResposta.length == 0){
-			construccioResposta = "No s'ha trobat el caracter '" + inputLletra + "' a la cadena!"
+		if (llistaResposta.length == 0) {
+			construccioResposta = "No s'ha trobat el caracter '" + inputLletra
+					+ "' a la cadena!"
 		} else if (llistaResposta.length == 1) {
-			construccioResposta = "Trobem la lletra '" + inputLletra + "' a la " + llistaResposta[0] + " posicio.";
-		} else if (llistaReposta.length == 2){
-			construccioResposta = "Trobem la lletra '" + inputLletra + "' a les posicions " + llistaResposta [0] + " i " + llistaResposta [1] + ".";
-		} else if (llistaResposta.length > 2){
-			construccioResposta = "Trobem la lletra " + inputLletra + " a les posicions " + llistaResposta [0];
-			for (var comptadorResposta = 1; comptadorResposta < llistaResposta.length - 2; comptadorResposta++){
+			construccioResposta = "Trobem la lletra '" + inputLletra
+					+ "' a la " + llistaResposta[0] + " posicio.";
+		} else if (llistaResposta.length == 2) {
+			construccioResposta = "Trobem la lletra '" + inputLletra
+					+ "' a les posicions " + llistaResposta[0] + " i "
+					+ llistaResposta[1] + ".";
+		} else if (llistaResposta.length > 2) {
+			construccioResposta = "Trobem la lletra '" + inputLletra
+					+ "' a les posicions " + llistaResposta[0];
+			for (var comptadorResposta = 1; comptadorResposta < llistaResposta.length - 1; comptadorResposta++) {
 				construccioResposta += ", " + llistaResposta[comptadorResposta];
 			}
-			construccioResposta += " i " + llistaResposta[llistaResposta.length] + ".";
+			construccioResposta += " i "
+					+ llistaResposta[llistaResposta.length - 1] + ".";
 		}
-		
+
 	}
 	return construccioResposta;
 }
@@ -98,6 +104,25 @@ function exercici7(input1, input2) {
 	return resultat
 }
 
+function exercici8(input1) {
+	var cadena = input1;
+	var coincidencies = 0;
+	var construccioResposta = "";
+	var detall = "";
 
+	for (var casella = 0; casella < cadena.length; casella++) {
 
+		for (var comptador09 = 0; comptador09 < 10; comptador09++) {
+			if (cadena.charAt(casella) == comptador09) {
+				coincidencies++;
+				detall += "<br>" + "Caracter " + comptador09 + " a la posicio "
+						+ casella + ".";
+			}
+		}
 
+	}
+	construccioResposta = "Donada la cadena " + cadena + ", s'han trobat "
+			+ coincidencies + " caracters numerics." + detall;
+	return construccioResposta
+
+}
